fd5bcc19530ff79e047fcc17d5ada3ffd12b3963
Artem Chebotko <artemchebotko@gmail.com>
2022-12-07 23:00:59 -0600

    Update wait.sh

--- wait.sh
+++ wait.sh
@@ -16,13 +16,12 @@ echo ""
 #echo "Choose Astra DB or Cassandra on the left panel to start."
 
 echo -n 'Starting a Cassandra cluster ...'; 
-#timeout 60 bash -c 'until cqlsh localhost 9042 -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
+timeout 60 bash -c 'until cqlsh localhost 9042 -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
 #sleep 2
-#timeout 60 bash -c 'until cqlsh localhost 9043 -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
+timeout 60 bash -c 'until cqlsh localhost 9043 -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
 #sleep 2
 #timeout 60 bash -c 'until ( cqlsh localhost 9042 -e "select count(*) as peers from system.peers;" | grep "(1 rows)" ) >/dev/null 2>&1; do sleep 1; echo -n "."; done'
 #sleep 2
-#timeout 60 bash -c 'until ( dc_count=$( docker exec -i -t Cassandra-1 bash -c "nodetool status" | grep -e "Datacenter" -c ) ; if [ $dc_count -eq 2 ]; then true; else false; fi ) >/dev/null 2>&1; do sleep 1; echo -n "."; done'
 
 timeout 60 bash -c 'dc_count=0; until [ $dc_count -eq 2 ] >/dev/null 2>&1; do sleep 1; dc_count=$( docker exec -i -t Cassandra-1 bash -c "nodetool status" | grep -e "Datacenter" -c ); echo -n "."; done'
 
